ae44b846df347c9d7089cc5a9e0f3ebf0251982f
sumit Bagga <sumit.bagga@gmail.com>
2025-06-25 20:23:11 +0100

    All test passing - Need to refactor the code

--- src/test/java/com/capgemini/rpnc/RpncApplicationTests.java
+++ src/test/java/com/capgemini/rpnc/RpncApplicationTests.java
@@ -38,19 +38,19 @@ class RpncApplicationTests {
 				Arguments.of("10 3 mod", "1.0"),
 				Arguments.of("6 3 /", "2.0"),
 				Arguments.of("2 3 + 5 *", "25.0"),
+				Arguments.of("1 0 /", "Infinity"),
 				//Negative Scenarios
-				Arguments.of("", ""),
+				Arguments.of("", " - Not a valid equation"),
 				Arguments.of(null, ""),
+				Arguments.of("1", "1 - Not a valid equation"),
+				Arguments.of("sin", "sin - Not a valid equation"),
+				Arguments.of("test", "test - Not a valid equation"),
 				Arguments.of("1 + 1", "1 + 1 - Not Reverse Polish Notation try backwards"),
-				Arguments.of("1", "1  Valid operation not found in equation"),
-				Arguments.of("sin", "sin - Not Reverse Polish Notation try backwards"),
-				Arguments.of("test - Valid operation not found in equation"),
-				Arguments.of("1 1 test - Valid operation not found in equation"),
-				Arguments.of("1 0 / - not sure at the moment"), // ToDo: Math error operation
-				Arguments.of("1 & 1 - Valid operation not found in equation"),
-				Arguments.of("1 1 + 5 # - Not a valid equation"),
-				Arguments.of("sin cos", "Not a valid equation"),
-				Arguments.of("+ -", "Not a valid equation")
+				Arguments.of("1 1 test", "1 1 test - Valid operation not found in equation"),
+				Arguments.of("1 & 1", "1 & 1 - Valid operation not found in equation"),
+				Arguments.of("1 1 + 5 #", "1 1 + 5 # - Not a valid equation"),
+				Arguments.of("sin cos", "sin cos - Not a valid equation"),
+				Arguments.of("+ -", "+ - - Not a valid equation")
 
 		);
 	}
