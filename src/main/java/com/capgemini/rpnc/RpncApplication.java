a989782a89345b32c5e04e8f860c3348702a009d
sumit Bagga <sumit.bagga@gmail.com>
2025-06-25 16:27:46 +0100

    Generate Maven Blank project from Spring.io

---
+++ src/main/java/com/capgemini/rpnc/RpncApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.rpnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/rpnc/RpncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.rpnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
